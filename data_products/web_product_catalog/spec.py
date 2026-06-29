448fb9cdce0ee299f33047d344f81e7c6563cda1
Diego García <diego.valverde@nextdata.com>
2026-06-29 13:25:27 +0200

    fix: enable temporary credentials

--- data_products/web_product_catalog/spec.py
+++ data_products/web_product_catalog/spec.py
@@ -40,7 +40,7 @@ spec = (
             storage("https://app.demo.trynxd.com/infra-profile/ecommerce-demo#/services/nxd-snowflake").config(
                 snowflake_config("RETAIL_CATALOG")
                 .target_table("CATALOG_PRODUCTS", catalog_products)
-                .enable_public_access()
+                .enable_temporal_credentials()
                 .disable_promotion()  # TODO: remove this after fixing the promotion issue: wrong table name in the promise context
             ),
         )
